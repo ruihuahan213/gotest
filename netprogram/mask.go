bf5fc81460dafde5c477c54976c3edd4f7b94ce3
unknown <ruihuahan213@hotmail.com>
2018-11-29 21:07:39 +0800

    mask.go added

---
+++ netprogram/mask.go
@@ -0,0 +1,36 @@
+﻿package main
+
+import (
+	"fmt"
+	"net"
+	"os"
+)
+
+func main() {
+
+	if len(os.Args) !=2 {
+		fmt.Fprintf(os.Stderr, "Usage: %s dotted-ip-addr\n", os.Args[0])
+		os.Exit(1)
+	}
+
+	var dotAddr string = os.Args[1]
+
+	var  addr net.IP = net.ParseIP(dotAddr)
+
+	if addr == nil {
+		fmt.Printf("Invalid address")
+		os.Exit(1)
+	}
+
+	var mask net.IPMask = addr.DefaultMask()
+	var network net.IP = addr.Mask(mask)
+	var ones, bits int = mask.Size()
+
+	fmt.Println(
+		"Address is ", addr.String(),
+		" Default mas length is ", bits,
+		" Leading ones count is ", ones,
+		" Mask is (hex) ", mask.String(),
+		" Network is ", network.String())
+	
+}
