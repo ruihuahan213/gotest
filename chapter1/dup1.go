a8cf75fb297f27624f2f4d8b49c7d564556cb742
unknown <ruihuahan213@hotmail.com>
2018-12-01 16:35:19 +0800

    dup1 added

---
+++ chapter1/dup1.go
@@ -0,0 +1,16 @@
+// Dup1 prints the text of each line that appears more than
+// once in the standard input, preceded by its count.
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+
+func main() {
+
+
+
+}
