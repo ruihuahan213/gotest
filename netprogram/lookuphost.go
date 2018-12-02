9414202df29ad718d73f6b69dfa92cefbbdaeebd
unknown <ruihuahan213@hotmail.com>
2018-12-02 10:01:34 +0800

    lookuphost.go added

---
+++ netprogram/lookuphost.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"net"
+	"os"
+	"fmt"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Fprintf(os.Stderr, "Usage: %s hostname\n", os.Args[0])
+		os.Exit(1)
+	}
+
+	name := os.Args[1]
+
+	addrs, err := net.LookupHost(name)
+	if err != nil {
+		fmt.Println("Error: ", err.Error())
+		os.Exit(1)
+	}
+
+	for _, s := range addrs {
+		fmt.Println(s)
+
+	}
+	os.Exit(0)
+}
