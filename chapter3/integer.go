9b15a00904918f88f3dbc0c820002b73836acd33
unknown <ruihuahan213@hotmail.com>
2018-12-11 19:32:37 +0800

    added

--- chapter3/integer.go
+++ chapter3/integer.go
@@ -34,4 +34,11 @@ func main() {
 		fmt.Println(medals[i]) // "bronze", "silver", "gold"
 	}	
 
+
+	ascii := 'a'
+	unicode := '国'
+	newline := '\n'
+	fmt.Printf("%d %[1]c %[1]q\n", ascii) // "97 a 'a'"
+	fmt.Printf("%d %[1]c %[1]q\n", unicode) // "22269 D 'D'"
+	fmt.Printf("%d %[1]q\n", newline) // "10 '\n'"
 }
